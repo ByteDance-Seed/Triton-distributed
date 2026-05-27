33eb8b0e0adfb7d3ee08cf6749bc64a9c8063f29
李诗宇 <lishiyu.314@bytedance.com>
2026-05-27 06:44:54 +0000

    fix: PP NCCL warm-up, Blackwell perf model, profiler num_blocks, ep_moe assert
    * warm up NCCL sub-group comms before PP forward
    * fix Blackwell perf model
    * Profiler.record(): add missing num_blocks arg
    * ep_moe_fused backward: relax assert == 8 to <= 8

--- python/triton_dist/tools/profiler/language.py
+++ python/triton_dist/tools/profiler/language.py
@@ -41,6 +41,7 @@ class Profiler:
     is_leader: tl.tensor
     group_id: tl.tensor
     num_groups: tl.constexpr
+    num_blocks: tl.tensor
 
     ENABLE_PROFILING: tl.constexpr
     NUM_BITS_ID: tl.constexpr
@@ -49,13 +50,20 @@ class Profiler:
     NUM_BITS_PAYLOAD: tl.constexpr
 
     @triton.jit
-    def create(profiler_buffer, group_id, num_groups=1, is_leader=True, num_blocks=None, ENABLE_PROFILING=True):
+    def create(
+        profiler_buffer,
+        group_id,
+        num_groups=1,
+        is_leader=True,
+        num_blocks=None,
+        ENABLE_PROFILING=True,
+    ):
         pid_size_x = tl.num_programs(axis=0)
         pid_size_y = tl.num_programs(axis=1)
         pid_size_z = tl.num_programs(axis=2)
         if num_blocks is None:
             num_blocks = pid_size_x * pid_size_y * pid_size_z
-        pid = tl.program_id(0) + tl.program_id(1) * pid_size_x + tl.program_id(2) * pid_size_x * pid_size_y
+        pid = (tl.program_id(0) + tl.program_id(1) * pid_size_x + tl.program_id(2) * pid_size_x * pid_size_y)
 
         NUM_SLOTS_GLOBAL_META: tl.constexpr = 1  # (num_blocks, num_groups) # noqa: F841
         NUM_SLOTS_BLOCK_META: tl.constexpr = 1  # (block_id, sm_id) # noqa: F841
@@ -74,18 +82,35 @@ class Profiler:
 
             profiler_buffer = profiler_buffer + 1 + num_blocks
         stride = num_blocks * num_groups
-        return Profiler(profiler_buffer + pid * num_groups + group_id, stride, to_tensor(group_id), num_groups,
-                        is_leader, num_blocks, ENABLE_PROFILING)
+        return Profiler(
+            profiler_buffer + pid * num_groups + group_id,
+            stride,
+            to_tensor(group_id),
+            num_groups,
+            is_leader,
+            num_blocks,
+            ENABLE_PROFILING,
+        )
 
-    def __init__(self, profiler_buffer, stride, group_id, num_groups, is_leader, num_blocks, ENABLE_PROFILING):
+    def __init__(
+        self,
+        profiler_buffer,
+        stride,
+        group_id,
+        num_groups,
+        is_leader,
+        num_blocks,
+        ENABLE_PROFILING,
+    ):
         """
-           is_leader: select a thread as leader to track start and end
+        is_leader: select a thread as leader to track start and end
         """
         self.group_id = group_id
         self.stride = stride
         self.buffer = profiler_buffer
         self.is_leader = is_leader
         self.num_groups = num_groups
+        self.num_blocks = num_blocks
         self.ENABLE_PROFILING = ENABLE_PROFILING
         self.NUM_BITS_ID = tl.constexpr(NUM_BITS_ID)
         self.NUM_BITS_PAYLOAD = tl.constexpr(NUM_BITS_PAYLOAD)
@@ -95,16 +120,16 @@ class Profiler:
     @triton.jit
     def encode_tag(self, is_start, task_type):
         """
-            TAG = GLOBAL ID | TASK TYPE | EVENT(IS_START)
+        TAG = GLOBAL ID | TASK TYPE | EVENT(IS_START)
         """
         tl.static_assert(self.NUM_BITS_TASK_TYPE + self.NUM_BITS_EVENT + self.NUM_BITS_ID <= 32)
         pid_size_x = tl.num_programs(axis=0)
         pid_size_y = tl.num_programs(axis=1)
-        pid = tl.program_id(0) + tl.program_id(1) * pid_size_x + tl.program_id(2) * pid_size_x * pid_size_y
+        pid = (tl.program_id(0) + tl.program_id(1) * pid_size_x + tl.program_id(2) * pid_size_x * pid_size_y)
         global_id = pid * self.num_groups + self.group_id
         GLOBAL_ID_OFFSET: tl.constexpr = self.NUM_BITS_TASK_TYPE + self.NUM_BITS_EVENT
         TASK_TYPE_OFFSET: tl.constexpr = self.NUM_BITS_EVENT
-        return (global_id << GLOBAL_ID_OFFSET) | (task_type << TASK_TYPE_OFFSET) | (is_start)
+        return ((global_id << GLOBAL_ID_OFFSET) | (task_type << TASK_TYPE_OFFSET) | (is_start))
 
     @triton.jit
     def get_timestamp(self):
@@ -126,4 +151,12 @@ class Profiler:
                 tl.store(self.buffer, entry)
             next_buffer = self.buffer + self.stride
             fence(semantic="sc", scope="cta")
-        return Profiler(next_buffer, self.stride, self.group_id, self.num_groups, self.is_leader, self.ENABLE_PROFILING)
+        return Profiler(
+            next_buffer,
+            self.stride,
+            self.group_id,
+            self.num_groups,
+            self.is_leader,
+            self.num_blocks,
+            self.ENABLE_PROFILING,
+        )
